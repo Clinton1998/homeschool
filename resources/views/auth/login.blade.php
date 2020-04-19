ce4edc8e00b648fc7f51bb3d01fe60204eff21a6
Antoni Luizar <antoni.luizar@gmail.com>
2020-04-19 13:05:57 -0500

    UI Login finalizado (Responsive)
    Pendientes:
    - Enlazar botón de Registrar mi Colegio
    - Si se quiere, cambiar el logo de HomeSchool
    - Revisión de funsionamiento

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
     <head>
         <meta charset="UTF-8">
@@ -8,88 +8,74 @@
         <title>TaskInnova</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,400i,600,700,800,900" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('assets/styles/css/themes/lite-purple.min.css')}}">
+
+        <link rel="stylesheet" href="{{asset('assets/styles/css/style-overwrite.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/styles/css/style.css')}}">
     </head>
 
     <body>
-        <div class="auth-layout-wrap" style="background-image: url({{asset('assets/images/photo-wide-4.jpeg')}})">
-            <div class="auth-content">
-                <div class="card o-hidden">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="p-4">
-                                <div class="auth-logo text-center mb-4">
-                                    <img src="{{asset('assets/images/logo.png')}}" alt="">
+        <div class="hs_container">
+            <div class="hs_logo-innova-container">
+                <img id="hs_logo-innova" src="{{asset('assets/images/Logo-Innova.png')}}" alt="">
+            </div>
+
+            <div class="hs_board col col-lg-8 " >
+                <div class="hs_wallpaper col col-lg-7 col-sm-6 col-mb-0 " style="background-image: url({{asset('assets/images/photo-wide-4.jpeg')}})">
+                </div>
+
+                <div class="hs_login col col-lg-5 col-sm-6 col-mb-12">
+
+                    <div class="hs_logo-container">
+                        <img class="hs_logo-homeschool" src="{{asset('assets/images/Logo-HomeSchool.png')}}" alt="">
+                    </div>
+                    <h1 class="mb-3 text-18 text-center">Ingresar a mi colegio</h1>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group text-left">
+                            <label for="email">Usuario</label>
+                            <input type="text" id="email"
+                                class="form-control form-control-rounded @error('email') is-invalid @enderror"
+                                name="email" value="{{ old('email') }}" required autocomplete="usuario"
+                                autofocus>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                        <div class="form-group text-left">
+                            <label for="password">Contraseña</label>
+                            <input type="password" id="password"
+                                class="form-control form-control-rounded @error('password') is-invalid @enderror"
+                                name="password" required autocomplete="current-password">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                        <div class="form-group text-right">
+                            <div class="">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember"
+                                        id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Recuérdame') }}
+                                    </label>
                                 </div>
-                                <h1 class="mb-3 text-18">Ingresar a mi colegio</h1>
-                                <form method="POST" action="{{ route('login') }}">
-                                    @csrf
-                                    <div class="form-group">
-                                        <label for="email">Usuario</label>
-                                        <input type="text" id="email"
-                                            class="form-control form-control-rounded @error('email') is-invalid @enderror"
-                                            name="email" value="{{ old('email') }}" required autocomplete="usuario"
-                                            autofocus>
-                                        @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="password">Contraseña</label>
-                                        <input type="password" id="password"
-                                            class="form-control form-control-rounded @error('password') is-invalid @enderror"
-                                            name="password" required autocomplete="current-password">
-                                        @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
-                                    </div>
-                                    <div class="form-group ">
-                                        <div class="">
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" name="remember"
-                                                    id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                                <label class="form-check-label" for="remember">
-                                                    {{ __('Recuérdame') }}
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <button class="btn btn-rounded btn-primary btn-block mt-2">Ingresar</button>
-
-                                </form>
-                                @if (Route::has('password.request'))
-
-                                <div class="mt-3 text-center">
-
-                                    <a href="{{ route('password.request') }}" class="text-muted"><u>¿Olvidó su contraseña?</u></a>
-                                </div>
-                                @endif
                             </div>
                         </div>
-                        <div class="col-md-6 text-center "
-                            style="background-size: cover;background-image: url({{asset('assets/images/photo-long-3.jpg')}}">
-                            <div class="pr-3 auth-right">
-                                @if (Route::has('register'))
-                                <a href="{{ route('register') }}"
-                                    class="btn btn-rounded btn-outline-primary btn-outline-email btn-block btn-icon-text">
-                                    <i class="i-Mail-with-At-Sign"></i> Sign up with Email
-                                </a>
-                                @endif
-                                <a
-                                    class="btn btn-rounded btn-outline-primary btn-outline-google btn-block btn-icon-text">
-                                    <i class="i-Google-Plus"></i> Sign up with Google
-                                </a>
-                                <a
-                                    class="btn btn-rounded btn-outline-primary btn-block btn-icon-text btn-outline-facebook">
-                                    <i class="i-Facebook-2"></i> Sign up with Facebook
-                                </a>
-                            </div>
-                        </div>
+
+                        <button class="btn btn-rounded btn-primary btn-block mt-2">Ingresar</button>
+                    </form>
+
+                    <div class="hs_login-footer">
+                        <button id="btn-registrar" class="btn btn-raised ripple btn-raised-secondary">Registrar mi colegio</button>
+                        
+                        @if (Route::has('password.request'))
+                            <a href="{{ route('password.request') }}" class="hs_olvide-contraseña text-muted"><u>Olvidé mi contraseña</u></a>
+                        @endif
                     </div>
                 </div>
             </div>
