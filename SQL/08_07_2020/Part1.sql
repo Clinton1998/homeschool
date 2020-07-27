41efb45d84b35452764ad2cd0ce6ad757d7fa5f4
Clinton1998 <clintontapialagar@gmail.com>
2020-07-27 09:52:53 -0500

    Archivos innecesarios eliminados

--- SQL/08_07_2020/Part1.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE colegio_m ADD COLUMN c_bloque_fact CHAR(5) COMMENT 'NOVIS = No visible, VISIB = Visible, VISME = Visible mensaje' AFTER c_clave ;
--- SQL/6-06-2020/Part1.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE preferencia_d ADD COLUMN b_datos_adicionales_calculo TINYINT(1) DEFAULT 0 AFTER id_usuario;
-ALTER TABLE preferencia_d ADD COLUMN c_modo_emision CHAR(3) DEFAULT 'DET' COMMENT 'DET = detallado,DIR = directo' AFTER b_datos_adicionales_calculo;
